7ecfe9fde7c6c2b9bca41cc1049ef715f93c39e1
Muhammad Rifki <mrifki028@gmail.com>
2023-08-20 02:36:02 +0700

    initial commit

---
+++ resources/js/Layouts/GuestLayout.jsx
@@ -0,0 +1,21 @@
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
+
+export default function Guest({ children }) {
+  return (
+    <div className="grid min-h-screen grid-cols-2 bg-gray-100 place-items-center">
+      <div className="flex flex-col items-center justify-center p-28">
+        <Link href="/">
+          <ApplicationLogo className="w-48 h-48 text-gray-500 fill-current" />
+        </Link>
+        <h1 className="mt-4 text-4xl font-semibold text-center">
+          Pengolahan Data-data Branch Operation Management
+        </h1>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full h-full overflow-hidden bg-white shadow-md sm:rounded-lg">
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ resources/js/Pages/Auth/Login.jsx
@@ -0,0 +1,100 @@
+import { useEffect } from "react";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
+
+export default function Login({ status, canResetPassword }) {
+  const { data, setData, post, processing, errors, reset } = useForm({
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    return () => {
+      reset("password");
+    };
+  }, []);
+
+  const handleOnChange = (event) => {
+    setData(
+      event.target.name,
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value
+    );
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    post(route("login"));
+  };
+
+  return (
+    <GuestLayout>
+      <Head title="Masuk" />
+
+      <div>
+        <h2 className="text-4xl font-semibold text-center">Masuk</h2>
+      </div>
+
+      {status && (
+        <div className="mb-4 text-sm font-medium text-green-600">{status}</div>
+      )}
+
+      <form
+        className="p-8 mt-8 border border-black rounded-xl"
+        onSubmit={submit}
+      >
+        <div>
+          <InputLabel htmlFor="email" value="Email" />
+
+          <TextInput
+            id="email"
+            type="email"
+            name="email"
+            value={data.email}
+            className="block w-full mt-1"
+            autoComplete="username"
+            isFocused={true}
+            onChange={handleOnChange}
+          />
+
+          <InputError message={errors.email} className="mt-2" />
+        </div>
+
+        <div className="mt-4">
+          <InputLabel htmlFor="password" value="Password" />
+
+          <TextInput
+            id="password"
+            type="password"
+            name="password"
+            value={data.password}
+            className="block w-full mt-1"
+            autoComplete="current-password"
+            onChange={handleOnChange}
+          />
+
+          <InputError message={errors.password} className="mt-2" />
+        </div>
+
+        <div className="flex flex-col items-center justify-end mt-4 gap-y-4">
+          <PrimaryButton disabled={processing}>Masuk</PrimaryButton>
+
+          {canResetPassword && (
+            <Link
+              href={route("password.request")}
+              className="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Forgot your password?
+            </Link>
+          )}
+        </div>
+      </form>
+    </GuestLayout>
+  );
+}
