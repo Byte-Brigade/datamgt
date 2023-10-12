229d1a202e5a0748a792da152c6a728011b32999
Setyo Bagus Nugraha <setyobagus01@gmail.com>
2023-10-12 11:58:19 +0700

    update: branch-detail

--- app/Http/Controllers/InqueryController.php
+++ app/Http/Controllers/InqueryController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Branch;
+use App\Models\EmployeePosition;
+use App\Models\OpsApar;
+use App\Models\OpsPajakReklame;
+use App\Models\OpsSkbirtgs;
+use App\Models\OpsSkOperasional;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -25,9 +30,41 @@ class InqueryController extends Controller
 
     public function branchDetail($id)
     {
-        $branch = Branch::where('branch_code', $id)->firstOrFail();
+        $branch = Branch::with('employees')->where('branch_code', $id)->firstOrFail();
+        $positions = EmployeePosition::get();
+
+        // Lisensi
+        $ops_skoperasional = OpsSkOperasional::where('branch_id', $branch->id)->first();
+        $ops_skbirtgs = OpsSkbirtgs::where('branch_id', $branch->id)->first();
+        $ops_pajak_reklame = OpsPajakReklame::where('branch_id', $branch->id)->first();
+        $ops_apar = OpsApar::where('branch_id', $branch->id)->first();
+        $lisensi = collect([
+            [
+                'name' => 'SK Operation',
+                'remark' => isset($ops_skoperasional) ? 'Ada' : 'Tidak Ada',
+                'jatuh_tempo' => isset($ops_skoperasional->expiry_date) ? $ops_skoperasional->expiry_date : '-'
+            ],
+            [
+                'name' => 'SK BI RTGS',
+                'remark' => isset($ops_skbirtgs) ? 'Ada' : 'Tidak Ada',
+                'jatuh_tempo' => '-'
+            ],
+            [
+                'name' => 'Reklame',
+                'remark' => isset($ops_pajak_reklame) ? 'Ada' : 'Tidak Ada',
+                'jatuh_tempo' => '-'
+            ],
+            [
+                'name' => 'APAR',
+                'remark' => isset($ops_apar) ? 'Ada' : 'Tidak Ada',
+                'jatuh_tempo' => isset($ops_apar->detail) ? $ops_apar->detail()->orderBy('expired_date', 'asc')->first()->expired_date : '-'
+            ],
+        ]);
+
         return Inertia::render('Inquery/Branch/Detail', [
-            'branch' => $branch
+            'branch' => $branch,
+            'positions' => $positions,
+            'licenses' => $lisensi
         ]);
     }
 }
