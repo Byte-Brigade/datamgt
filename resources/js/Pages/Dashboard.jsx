1976771c196603c66d95c9cb4efb216eac59ba6d
Muhammad Rifki <mrifki028@gmail.com>
2023-11-16 17:17:10 +0700

    fix: styling

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -220,11 +220,7 @@ export default function Dashboard({
           <div className="grid grid-cols-4 gap-x-4">
             <div
               onClick={() => setActive("cabang")}
-              className={`flex items-center px-4 py-2 border cursor-pointer gap-x-4 border-slate-400 rounded-xl ${
-                active === "cabang"
-                  ? "hover:bg-slate-200 bg-slate-100"
-                  : "bg-white hover:bg-slate-200"
-              } transition-all duration-300 focus:ring-2`}
+              className="flex items-center px-4 py-2 bg-white border cursor-pointer gap-x-4 border-slate-400 rounded-xl"
             >
               <BuildingOffice2Icon className="w-10 h-10" />
               <div className="flex flex-col">
@@ -560,67 +556,6 @@ export default function Dashboard({
                   className="[&>td]:p-2 cursor-pointer font-bold text-cyan-600 hover:bg-slate-200 border-b divide-x divide-slate-200 border-slate-200"
                 >
                   <td colSpan={2}>Kantor Cabang</td>
-<<<<<<< HEAD
-                  {data.summary_assets["Kantor Pusat"] &&
-                    Object.entries(data.summary_assets["Kantor Pusat"]).map(
-                      ([key, item]) =>
-                        key === "Depre" ? (
-                          <>
-                            <td className="text-center">
-                              {
-                                data.assets.filter(
-                                  (item) =>
-                                    item.branch_name !== "Kantor Pusat" &&
-                                    item.category === "Depre"
-                                ).length
-                              }
-                            </td>
-                            <td className="text-right">
-                              {data.assets
-                                .filter(
-                                  (item) =>
-                                    item.branch_name !== "Kantor Pusat" &&
-                                    item.category === "Depre"
-                                )
-                                .reduce((total, item) => {
-                                  return total + item.asset_cost;
-                                }, 0)
-                                .toLocaleString("id-ID")}
-                            </td>
-
-                            <td className="text-center"></td>
-
-                            <td className="text-center">
-                              {data.assets
-                                .filter(
-                                  (item) =>
-                                    item.branch_name !== "Kantor Pusat" &&
-                                    item.category === "Depre"
-                                )
-                                .reduce((total, item) => {
-                                  return total + item.net_book_value;
-                                }, 0)
-                                .toLocaleString("id-ID")}
-                            </td>
-                          </>
-                        ) : (
-                          <>
-                            <td className="text-center">
-                              {
-                                data.assets.filter(
-                                  (item) =>
-                                    item.branch_name !== "Kantor Pusat" &&
-                                    item.category === "Non-Depre"
-                                ).length
-                              }
-                            </td>
-                            <td className="text-center">
-                              {item.nilai_perolehan.toLocaleString("id-ID")}
-                            </td>
-                          </>
-                        )
-                    )}
-=======
 
                           <td className="text-center">
                             {
@@ -678,7 +613,6 @@ export default function Dashboard({
                           </td>
 
 
->>>>>>> 347eea25ffebd780c4aab3190540a9b9de4318fb
                 </tr>
 
                 {open &&
