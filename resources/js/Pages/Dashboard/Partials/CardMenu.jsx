8b4bf819e9a77b44f5637994ea3369b7cc731afc
Setyo Bagus Nugraha <56948098+setyobagus01@users.noreply.github.com>
2023-11-20 13:55:58 +0700

    add scoring data

--- resources/js/Pages/Dashboard/Partials/CardMenu.jsx
+++ resources/js/Pages/Dashboard/Partials/CardMenu.jsx
@@ -15,48 +15,48 @@ export default function CardMenu({
     color === "blue"
       ? "text-cyan-800"
       : color === "green"
-      ? "text-green-800"
-      : color === "orange"
-      ? "text-orange-800"
-      : color === "purple"
-      ? "text-deep-purple-800"
-      : "";
+        ? "text-green-800"
+        : color === "orange"
+          ? "text-orange-800"
+          : color === "purple"
+            ? "text-deep-purple-800"
+            : "";
 
   const outlineColor =
     color === "blue"
       ? "bg-cyan-100"
       : color === "green"
-      ? "bg-green-100"
-      : color === "orange"
-      ? "bg-orange-100"
-      : color === "purple"
-      ? "bg-deep-purple-100"
-      : "bg-slate-100";
+        ? "bg-green-100"
+        : color === "orange"
+          ? "bg-orange-100"
+          : color === "purple"
+            ? "bg-deep-purple-100"
+            : "bg-slate-100";
 
   const ringColor =
     color === "blue"
       ? "ring-cyan-500"
       : color === "green"
-      ? "ring-green-500"
-      : color === "orange"
-      ? "ring-orange-500"
-      : color === "purple"
-      ? "ring-deep-purple-500"
-      : "ring-cyan-500";
+        ? "ring-green-500"
+        : color === "orange"
+          ? "ring-orange-500"
+          : color === "purple"
+            ? "ring-deep-purple-500"
+            : "ring-cyan-500";
 
   const setData =
     type === "branch"
       ? branchState
         ? data.branches.filter(
-            (branch) =>
-              (branch.id == branchState && areaState === "none") ||
-              branch.area === area
-          ).length
+          (branch) =>
+            (branch.id == branchState && areaState === "none") ||
+            branch.area === area
+        ).length
         : data.branches.filter(
-            (branch) => areaState === "none" || branch.area === areaState
-          ).length
+          (branch) => areaState === "none" || branch.area === areaState
+        ).length
       : type === "atm"
-      ? Object.keys(data.jumlah_atm).reduce((acc, atm) => {
+        ? Object.keys(data.jumlah_atm).reduce((acc, atm) => {
           return (
             acc +
             data.jumlah_atm[atm].filter(
@@ -66,32 +66,37 @@ export default function CardMenu({
             ).length
           );
         }, 0)
-      : type === "employee"
-      ? branchState
-        ? data.jumlahKaryawan.filter(
-            (employee) =>
-              employee.branch_id == branchState &&
-              (areaState === "none" || employee.branches.area === areaState)
-          ).length
-        : data.jumlahKaryawan.filter(
-            (employee) => areaState === "none" || employee.branches.area === areaState
-          ).length
-      : type === "asset"
-      ? data.assets.filter(
-          (asset) =>
-            (branchState === 0 || asset.branch_id == branchState) &&
-            (areaState === "none" || asset.branches.area == areaState)
-        ).length
-      : [];
+        : type === "employee"
+          ? branchState
+            ? data.jumlahKaryawan.filter(
+              (employee) =>
+                employee.branch_id == branchState &&
+                (areaState === "none" || employee.branches.area === areaState)
+            ).length
+            : data.jumlahKaryawan.filter(
+              (employee) => areaState === "none" || employee.branches.area === areaState
+            ).length
+          : type === "asset"
+            ? data.assets.filter(
+              (asset) =>
+                (branchState === 0 || asset.branch_id == branchState) &&
+                (areaState === "none" || asset.branches.area == areaState)
+            ).length
+            : type === "gap_scorings"
+              ? data.gap_scorings.filter(
+                (asset) =>
+                  (branchState === 0 || asset.branch_id == branchState) &&
+                  (areaState === "none" || asset.branches.area == areaState)
+              ).length
+              : [];
 
   return (
     <div
       onClick={onClick}
-      className={`flex items-center px-4 py-2 border cursor-pointer gap-x-4 border-slate-400 rounded-lg ${
-        active === type
+      className={`flex items-center px-4 py-2 border cursor-pointer gap-x-4 border-slate-400 rounded-lg ${active === type
           ? "hover:bg-slate-200 bg-slate-100 ring-2 ring-offset-2 " + ringColor
           : "bg-white hover:bg-slate-200"
-      } transition-all duration-300`}
+        } transition-all duration-300`}
     >
       <div className={`p-2 rounded-full ${outlineColor}`}>
         <Icon className={`w-8 h-8 ${iconColor}`} />
