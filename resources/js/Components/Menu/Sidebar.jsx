b81bbb5cca9707c50a76b1e6b941ff13d0f669f6
setyobagus01 <56948098+setyobagus01@users.noreply.github.com>
2024-02-20 09:06:22 +0700

    Update Sidebar.jsx

--- resources/js/Components/Menu/Sidebar.jsx
+++ resources/js/Components/Menu/Sidebar.jsx
@@ -4,6 +4,7 @@ import {
   ChevronDownIcon,
   ChevronRightIcon,
   CircleStackIcon,
+  DocumentTextIcon,
   FolderIcon,
   HomeIcon,
   PresentationChartBarIcon,
@@ -73,14 +74,19 @@ export function SidebarWithLogo({ sidebarOpen, setSidebarOpen }) {
     { name: "Asset", path: "inquery.assets" },
     { name: "Alih Daya", path: "maintenance" },
     // { name: "Lisensi", path: "inquery.licenses" },
-    { name: "Branch (Report)", path: "reporting.branches", state: "reporting.*" },
-    { name: "Asset (Report)", path: "reporting.assets" },
+    // { name: "Branch (Report)", path: "reporting.branches", state: "reporting.*" },
+    // { name: "Asset (Report)", path: "reporting.assets" },
     // { name: "Lisensi (Report)", path: "reporting.licenses" },
-    { name: "Vendor (Report)", path: "reporting.vendor" },
-    { name: "Branch Roll Out (Report)", path: "reporting.bros" },
+    // { name: "Vendor (Report)", path: "reporting.vendor" },
+    // { name: "Branch Roll Out (Report)", path: "reporting.bros" },
   ];
 
   const reportRouter = [
+    { name: "Branch", path: "reporting.branches", state: "reporting.*" },
+    { name: "Asset", path: "reporting.assets" },
+    { name: "Lisensi", path: "reporting.licenses" },
+    { name: "Vendor", path: "reporting.vendor" },
+    { name: "Branch Roll Out", path: "reporting.bros" },
   ];
 
   const opsRouter = [
@@ -266,6 +272,89 @@ export function SidebarWithLogo({ sidebarOpen, setSidebarOpen }) {
             </List>
           </AccordionBody>
         </Accordion>
+
+        {auth.role !== "cabang" && (
+          <>
+            {/* Report */}
+            <hr className="my-2 border-blue-gray-50" />
+            <Accordion
+              open={openAcc2}
+              className={`${sidebarOpen && "w-12"}`}
+              icon={
+                !sidebarOpen && (
+                  <ChevronDownIcon
+                    strokeWidth={2.5}
+                    className={`mx-auto h-4 w-4 transition-transform ${openAcc2 ? "rotate-180" : ""
+                      }`}
+                  />
+                )
+              }
+            >
+              <ListItem className="p-0" selected={openAcc2}>
+                <Tooltip
+                  content="Report"
+                  placement="right"
+                  className={`${!sidebarOpen && "hidden"}`}
+                >
+                  <AccordionHeader
+                    onClick={() => {
+                      handleOpenAcc(2);
+                      sidebarOpen && setSidebarOpen(!sidebarOpen);
+                    }}
+                    className={`p-3 border-b-0 [&>span]:m-0 justify-center`}
+                  >
+                    <ListItemPrefix className={`${sidebarOpen && "m-0"}`}>
+                      <DocumentTextIcon className="w-5 h-5" />
+                    </ListItemPrefix>
+                    {!sidebarOpen && (
+                      <Typography
+                        color="blue-gray"
+                        className={`mr-auto font-normal`}
+                      >
+                        Report
+                      </Typography>
+                    )}
+                  </AccordionHeader>
+                </Tooltip>
+              </ListItem>
+              <AccordionBody className="py-1">
+                <List
+                  className={`p-0 ${!sidebarOpen ? "min-w-[200px]" : "min-w-0"
+                    }`}
+                >
+                  {reportRouter.map((router, index) => (
+                    <Tooltip
+                      key={index}
+                      content={router.name}
+                      placement="right"
+                      className={`${!sidebarOpen && "hidden"}`}
+                    >
+                      <Link href={route(router.path)}>
+                        <ListItem
+                          className={`${sidebarOpen && "justify-center"}`}
+                          selected={
+                            route().current(router.path) ||
+                            route().current(`${router.path}.*`)
+                          }
+                        >
+                          <ListItemPrefix className={`${sidebarOpen && "m-0"}`}>
+                            <ChevronRightIcon
+                              strokeWidth={3}
+                              className={`w-5 h-3 ${sidebarOpen && "my-1"}`}
+                            />
+                          </ListItemPrefix>
+                          {!sidebarOpen && (
+                            <Typography>{router.name}</Typography>
+                          )}
+                        </ListItem>
+                      </Link>
+                    </Tooltip>
+                  ))}
+                </List>
+              </AccordionBody>
+            </Accordion>
+          </>
+        )}
 
 
 
