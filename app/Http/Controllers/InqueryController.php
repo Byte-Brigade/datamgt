b1c4eaade29adca475d207132c9e68bbae706c7e
setyobagus01 <56948098+setyobagus01@users.noreply.github.com>
2024-02-20 09:02:26 +0700

    Update InqueryController.php

--- app/Http/Controllers/InqueryController.php
+++ app/Http/Controllers/InqueryController.php
@@ -29,6 +29,69 @@ class InqueryController extends Controller
     {
         return Inertia::render('Inquery/Branch/Page');
     }
+
+    public function branchDetail($slug)
+    {
+
+        $branch = Branch::with(['employees', 'gap_alih_dayas' => function ($query) {
+            $latestPeriode = $query->max('periode');
+            return $query->where('periode', $latestPeriode);
+        }])->where('slug', $slug)->firstOrFail();
+        $positions = EmployeePosition::get();
+
+        // Lisensi
+        $ops_skoperasional = OpsSkOperasional::where('branch_id', $branch->id)->first();
+        $ops_skbirtgs = OpsSkbirtgs::where('branch_id', $branch->id)->first();
+        $ops_pajak_reklame = OpsPajakReklame::where('branch_id', $branch->id)->first();
+        $ops_apar = OpsApar::where('branch_id', $branch->id)->first();
+
+        $izin_disnaker = GapDisnaker::where('branch_id', $branch->id)->orderBy('tgl_masa_berlaku', 'asc')->get()->map(function ($disnaker) {
+            return [
+                'name' => $disnaker->jenis_perizinan->name,
+                'remark' => 'Ada',
+                'jatuh_tempo' => $disnaker->tgl_masa_berlaku,
+                'url' => isset($disnaker->file) ? "infra/disnaker/{$disnaker->id}/{$disnaker->file}" : false,
+
+            ];
+        });
+        $lisensi = collect([
+            [
+                'name' => 'Izin OJK',
+                'remark' => isset($branch->izin) ? 'Ada' : 'Tidak Ada',
+                'jatuh_tempo' => '-',
+                'url' => isset($branch->file_ojk) ? "ops/branches/{$branch->id}/{$branch->file_ojk}" : false,
+
+            ],
+            [
+                'name' => 'SK BI RTGS',
+                'remark' => isset($ops_skbirtgs) ? 'Ada' : 'Tidak Ada',
+                'jatuh_tempo' => '-',
+                'url' => isset($ops_skbirtgs->file) ? "ops/skbirtgs/{$ops_skbirtgs->id}/{$ops_skbirtgs->file}" : false,
+            ],
+            [
+                'name' => 'Reklame',
+                'remark' => isset($ops_pajak_reklame) ? 'Ada' : 'Tidak Ada',
+                'jatuh_tempo' => isset($ops_pajak_reklame->periode_akhir) ? $ops_pajak_reklame->periode_akhir : '-',
+                'url' => isset($ops_pajak_reklame->file_izin_reklame) ? "ops/pajak-reklame/{$ops_pajak_reklame->id}/{$ops_skbirtgs->file_izin_reklame}" : false,
+
+            ],
+            [
+                'name' => 'APAR',
+                'remark' => isset($ops_apar) ? 'Ada' : 'Tidak Ada',
+                'jatuh_tempo' => isset($ops_apar->detail) ? $ops_apar->detail()->orderBy('expired_date', 'asc')->first()->expired_date : '-'
+            ],
+        ]);
+
+        $lisensi = $lisensi->merge($izin_disnaker);
+
+
+
+        return Inertia::render('Inquery/Branch/Detail', [
+            'branch' => $branch,
+            'positions' => $positions,
+            'licenses' => $lisensi
+        ]);
+    }
     public function staff()
     {
         return Inertia::render('Inquery/Staff/Page');
